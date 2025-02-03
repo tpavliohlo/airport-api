7e962c0cfdb7602f646e99864ef648f5e43f8d08
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-02-03 21:50:02 +0100

    update admin page

--- flights/admin.py
+++ flights/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from flights.models import Flight
+from flights.models import Flight, Crew, Route, Airport, Order, Ticket, AirplaneType, Airplane
 
 admin.site.register(Flight)
+admin.site.register(Airplane)
+admin.site.register(AirplaneType)
+admin.site.register(Ticket)
+admin.site.register(Order)
+admin.site.register(Route)
+admin.site.register(Airport)
+admin.site.register(Crew)
