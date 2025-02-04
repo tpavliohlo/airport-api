417f51e4c09851eb55d960101a098ee773b5c308
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-02-04 17:50:04 +0100

    Create url and view for Airplane List

--- flights/urls.py
+++ flights/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from flights.views import FlightViewSet
+from flights.views import FlightViewSet, AirplaneViewSet
 
 router = routers.DefaultRouter()
 router.register("flights", FlightViewSet)
-
+router.register("airplanes", AirplaneViewSet)
 urlpatterns = [path("", include(router.urls))]
 
 app_name = "flight"
--- flights/views.py
+++ flights/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from rest_framework.viewsets import GenericViewSet
 
-from flights.models import Flight
-from flights.serializers import FlightSerializer
+from flights.models import Flight, Airplane
+from flights.serializers import FlightSerializer, AirplaneSerializer
 
 
 class FlightViewSet(
@@ -13,3 +13,13 @@ class FlightViewSet(
 ):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+
+
+class AirplaneViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
