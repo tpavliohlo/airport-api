0b50872668bf75214a56884fcf47e571c7a613dc
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-02-10 16:44:04 +0100

    Implementation of Token authentication

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'flights',
     'rest_framework',
+    'rest_framework.authtoken',
     # 'user'
 ]
 
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
-from user.views import CreateUserView
-
+from user.views import CreateUserView, LoginUserView
 
 urlpatterns = [
-    path('register/', CreateUserView.as_view(), name='create')
+    path('register/', CreateUserView.as_view(), name='create'),
+    path('login/', LoginUserView.as_view(), name='get_token'),
 ]
 
 app_name = 'user'
