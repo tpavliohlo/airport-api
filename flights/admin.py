6612350740a11dfc61f785c5c8ced11d394c7d13
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-02-10 10:42:01 +0100

    create class TicketInLine & OrderAdmin in admin.py

--- flights/admin.py
+++ flights/admin.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 
 from flights.models import Flight, Crew, Route, Airport, Order, Ticket, AirplaneType, Airplane
 
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [TicketInLine,]
+
+
 admin.site.register(Flight)
 admin.site.register(Airplane)
 admin.site.register(AirplaneType)
 admin.site.register(Ticket)
-admin.site.register(Order)
 admin.site.register(Route)
 admin.site.register(Airport)
 admin.site.register(Crew)
