5cec65d59393a5370ae6bcaf4e3af575b87f6459
Tymofii Pavliohlo <tim13fiz@gmail.com>
2025-02-11 22:00:32 +0100

    Install JWT-authentication & implementation it in class ManageUserView

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'flights',
     'rest_framework',
     'rest_framework.authtoken',
-    # 'user'
+     # 'user'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from user.views import CreateUserView, LoginUserView, ManageUserView
 
 urlpatterns = [
     path('register/', CreateUserView.as_view(), name='create'),
     path('login/', LoginUserView.as_view(), name='get_token'),
-    path('me/', ManageUserView.as_view(), name='manage_user')
+    path('me/', ManageUserView.as_view(), name='manage_user'),
+    path("login/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 app_name = 'user'
